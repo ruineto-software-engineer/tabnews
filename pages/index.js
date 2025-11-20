6933663a4644ebb508552bf7a2fea18865454989
Rui Neto <ruineto11@gmail.com>
2025-11-20 11:52:58 -0300

    Update the homepage content

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <>
-      <h1>Meu amorzinho, totozo!</h1>
+      <h1>❤️ Meu amorzinho, totozo! ❤️</h1>
       <p>Eu sempre irei cuidar de você, e para toda vida, sempre te amarei!</p>
       <p>E se você me ama dá uma risadinha!</p>
     </>
