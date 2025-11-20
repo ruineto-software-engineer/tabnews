bafb77efaed0a7a0c0feb07a2f20a9582efcb4f7
Rui Neto <ruineto11@gmail.com>
2025-11-20 10:44:41 -0300

    Add homepage

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <>
+      <h1>Meu amorzinho, totozo!</h1>
+      <p>Eu sempre irei cuidar de você, e para toda vida, sempre te amarei!</p>
+      <p>E se você me ama dá uma risadinha!</p>
+    </>
+  );
+}
+
+export default Home;
