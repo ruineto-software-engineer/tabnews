e955f26dcf06ae783ced71dde241c6f140903418
Rui Neto <ruineto11@gmail.com>
2025-12-12 14:06:36 -0300

    update database configuration to use SSL setting from environment variables

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.POSTGRES_SSL === "false" ? false : true,
   });
   console.log("Credenciais do Postgre:", {
     host: process.env.POSTGRES_HOST,
@@ -14,7 +15,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: process.env.POSTGRES_SSL === "false" ? false : true,
   });
 
   try {
