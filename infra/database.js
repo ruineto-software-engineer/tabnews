6369606e1eed87ae8aba686de41b9ba12f882352
Rui Neto <ruineto11@gmail.com>
2025-12-14 11:24:15 -0300

    adds migration scripts

--- infra/database.js
+++ infra/database.js
@@ -9,14 +9,6 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
     ssl: process.env.POSTGRES_SSL_MODE === "disable" ? false : true,
   });
-  console.log("Credenciais do Postgre:", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.POSTGRES_SSL_MODE === "disable" ? false : true,
-  });
 
   try {
     await client.connect();
